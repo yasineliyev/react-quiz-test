78b494667fd1d8cf7832a963ec9e0d9b5dd13741
Yasin <105815296+yasineliyev@users.noreply.github.com>
2023-01-24 13:56:05 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { questions } from "./questionsData";
 
 function App() {
+  const [showFinalResult, setShowFinalResult] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowFinalResult(true);
+    }
+  };
+
+  const restartTest = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowFinalResult(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Bilik testi</h1>
+        <h2>Topladığın xal: {score}</h2>
+        {showFinalResult ? (
+          <div className="result">
+            <h1>Nəticə</h1>
+            <h2>
+              Doğru cavab: {score} ({(score / questions.length) * 100}%)
+            </h2>
+            <button onClick={() => restartTest()}>Yenidən başla</button>
+          </div>
+        ) : (
+          <div className="question-card">
+            <h2>
+              Ümumi sual sayı: {questions.length} <br />
+              İndiki sual: {currentQuestion + 1}
+            </h2>
+            <h3>{questions[currentQuestion].text}</h3>
+            <ul>
+              {questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    onClick={() => optionClicked(option.isCorrect)}
+                    key={option.id}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/questionsData.js
@@ -0,0 +1,91 @@
+export const questions = [
+  {
+    text: "Yaponiyanın paytaxtı haradır?",
+    options: [
+      { id: 0, text: "Praqa", isCorrect: false },
+      { id: 1, text: "Tokio", isCorrect: true },
+      { id: 2, text: "Pekin", isCorrect: false },
+      { id: 3, text: "Seul", isCorrect: false },
+    ],
+  },
+  {
+    text: "Aşağıdakı sözlərdən hansı düzgün yazılıb?",
+    options: [
+      { id: 0, text: "mundəricat", isCorrect: false },
+      { id: 1, text: "mütərəqqi", isCorrect: true },
+      { id: 2, text: "ədəbiyat", isCorrect: false },
+      { id: 3, text: "lanpa", isCorrect: false },
+    ],
+  },
+  {
+    text: "Əhalisi ən çox olan ölkə hansıdır?",
+    options: [
+      { id: 0, text: "Pakistan", isCorrect: false },
+      { id: 1, text: "Hindistan", isCorrect: false },
+      { id: 2, text: "ABŞ", isCorrect: false },
+      { id: 3, text: "Çin", isCorrect: true },
+    ],
+  },
+  {
+    text: "Azərbaycan Respublikasının Dövlət Himninin musiqisi kimə aiddir?",
+    options: [
+      { id: 0, text: "Əhməd Cavad", isCorrect: false },
+      { id: 1, text: "Üzeyir Hacıbəyov", isCorrect: true },
+      { id: 2, text: "Müslüm Maqomayev", isCorrect: false },
+    ],
+  },
+  {
+    text: 'İngiliscə "famous" sözünün sinonimi hansıdır?',
+    options: [
+      { id: 0, text: "wellness", isCorrect: false },
+      { id: 1, text: "wellhead", isCorrect: false },
+      { id: 2, text: "well-known", isCorrect: true },
+      { id: 3, text: "well-off", isCorrect: false },
+    ],
+  },
+  {
+    text: "Bu klublardan hansı ispan futbol klubudur?",
+    options: [
+      { id: 0, text: "Salernitana", isCorrect: false },
+      { id: 1, text: "Leganes", isCorrect: true },
+      { id: 2, text: "Bari", isCorrect: false },
+      { id: 3, text: "Le Havre", isCorrect: false },
+    ],
+  },
+  {
+    text: "Peru hansı materikdə yerləşir?",
+    options: [
+      { id: 0, text: "Cənubi Amerika", isCorrect: true },
+      { id: 1, text: "Şimali Amerika", isCorrect: false },
+      { id: 2, text: "Afrika", isCorrect: false },
+      { id: 3, text: "Avrasiya", isCorrect: false },
+    ],
+  },
+  {
+    text: "Məhəmməd Füzuli hansı şəhərdə anadan olub?",
+    options: [
+      { id: 0, text: "Səmərqənd", isCorrect: false },
+      { id: 1, text: "Tehran", isCorrect: false },
+      { id: 2, text: "Bağdad", isCorrect: false },
+      { id: 3, text: "Kərbəla", isCorrect: true },
+    ],
+  },
+  {
+    text: "Adı, paytaxt adı ilə eyni olan ölkə hansıdır?",
+    options: [
+      { id: 0, text: "Braziliya", isCorrect: false },
+      { id: 1, text: "Əlcəzair", isCorrect: true },
+      { id: 2, text: "İraq", isCorrect: false },
+      { id: 3, text: "Hindistan", isCorrect: false },
+    ],
+  },
+  {
+    text: "Azərbaycanda ən yüksək dağ zirvəsi hansıdır?",
+    options: [
+      { id: 0, text: "Şahdağ", isCorrect: false },
+      { id: 1, text: "Tufandağ", isCorrect: false },
+      { id: 2, text: "Bazaryurd", isCorrect: false },
+      { id: 3, text: "Bazardüzü", isCorrect: true },
+    ],
+  },
+];
